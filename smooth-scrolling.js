10eb43c1a5205f7d2152621efbc7828a27e92a1c
Baptiste Briel <baptiste@sa-studio.fr>
2016-05-24 10:21:07 +0200

    2.1.9

--- smooth-scrolling.js
+++ smooth-scrolling.js
@@ -127,7 +127,7 @@ var Smooth = function () {
         key: 'calc',
         value: function calc(e) {
 
-            var delta = this.direction == 'horizontal' ? e.deltaX : e.deltaY;
+            var delta = this.vars.direction == 'horizontal' ? e.deltaX : e.deltaY;
 
             this.vars.target += delta * -1;
             this.vars.target = Math.max(0, Math.min(this.vars.target, this.vars.bounding));
@@ -184,7 +184,7 @@ var Smooth = function () {
         key: 'getTransform',
         value: function getTransform(value) {
 
-            return this.direction === 'vertical' ? 'translate3d(0,' + value + 'px,0)' : 'translate3d(' + value + 'px,0,0)';
+            return this.vars.direction === 'vertical' ? 'translate3d(0,' + value + 'px,0)' : 'translate3d(' + value + 'px,0,0)';
         }
     }, {
         key: 'on',
